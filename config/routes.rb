566c925fa51f6983b91b5fac2cbe6554d935089f
Michael Ziemianski <mhziemianski@gmail.com>
2015-07-11 12:16:26 -0500

    Customize bootstrap variables

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home" 
-  get "about" => "pages#about"
-  # The priority is based upon order of creation: first created -> highest priority.
+  get "about" => "pages#about#"
+  get "mission" => "pages#mission"
+      # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
