4ba2935ec6900d2b9fc04cfe65afb29be74a9a14
Michael Ziemianski <mhziemianski@gmail.com>
2015-09-03 11:44:00 -0500

    modified pins_controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :unless => :devise_controller?, except: [:index, :show]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
   def index
